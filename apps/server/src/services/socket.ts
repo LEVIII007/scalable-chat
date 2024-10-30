cd11a821188b458236de59ab75c0c838e428204f
“Shashank <shashanktyagiji12345@gmail.com>
2024-10-30 20:08:37 +0530

    feat: Add iovalkey to web package dependencies

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,5 +1,23 @@
 import { Server } from 'socket.io';
 import http from 'http';
+import {Redis} from 'iovalkey';
+
+const pub = new Redis(
+    {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : undefined,
+        username: process.env.REDIS_USERNAME,
+        password: process.env.REDIS_PASSWORD,
+    }
+)
+const sub = new Redis(
+    {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : undefined,
+        username: process.env.REDIS_USERNAME,
+        password: process.env.REDIS_PASSWORD,
+    }
+);
 
 class SocketService {
     private _io: Server;
@@ -8,12 +26,19 @@ class SocketService {
         console.log('SocketService constructor');
         const server = http.createServer();
         this._io = new Server(server, {
+            cors: {
+                origin: '*',
+                allowedHeaders: '*',
+            },
             // Add any necessary options here
         });
+        sub.subscribe('MESSAGES');
+
 
     }
 
     public initListeners() {
+        const io = this._io;
         console.log('SocketService initListeners');
         this._io.on('connect', (socket) => {
             console.log('new Socket connected', socket.id);
@@ -25,11 +50,19 @@ class SocketService {
 
             socket.on('event:message', async({message} : {message : string}) => {
                 console.log('message', message);
+                await pub.publish('MESSAGES', JSON.stringify({message}));
                 // socket.emit('event:message', {message});
             }
+            
             );
 
         });
+
+        sub.on('message', async (channel, message) => {
+            if(channel === 'MESSAGES') {
+                console.log('message received', message);
+                io.emit('event:message', JSON.parse(message));
+            }
     }
 
 
